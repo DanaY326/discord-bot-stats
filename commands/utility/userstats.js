f0c5190ad04813d1388a60fabf66141081c95d8e
Dana Yuan <danayuan2006@gmail.com>
2025-04-30 17:43:55 -0400

    Implemented userstats fully w/o ephemeral (real)

--- commands/utility/userstats.js
+++ commands/utility/userstats.js
@@ -13,7 +13,7 @@ module.exports = {
 		.setDescription('Returns the biggest contributers (yappers) in the server!')
 		.addIntegerOption(option =>
 			option.setName('number_of_users')
-				  .setDescription('The number of top users to give data for (default 10).')),
+				  .setDescription('The maximum number of top users to give data for (default 10).')),
     async execute(interaction) {
         const serverName = `${interaction.guild.name}`;
 
@@ -44,13 +44,17 @@ module.exports = {
 				const numMessages = Object.values(numMessagesRes.recordset[0]);
 				//console.log(numMessages);
 				
-				const numUsersRes = await sql.query`SELECT COUNT(id) FROM dbo.Users`;
+				const numUsersRes = await sql.query`SELECT COUNT(DISTINCT userId) FROM dbo.Messages`;
 				//console.log(numUsersRes);
 				const numUsers = Object.values(numUsersRes.recordset[0]);
-				//console.log(numUsers);
+				console.log(numUsers);
 
-				const top = interaction.options.getInteger('number_of_users') ?? 10;
+				const top = Math.min(numUsers, interaction.options.getInteger('number_of_users') ??  10);
 				//console.log(top);
+			// make this and other error messages ephemeral
+				if (top <= 0) {
+					return interaction.reply(`No data available for "number_of_users" less than 1!`);
+				}
 /*
 				await sql.query``;
 				await sql.query``;*/
@@ -75,7 +79,12 @@ module.exports = {
 
 					reply = reply + `\n(${mesNum / numMessages * 100}%)       ${Object.values(userObj)}`;
 				}
-				return interaction.reply(`Users by percentage of messages: ${reply}`);
+				if (top == numUsers) {
+					return interaction.reply(`${top} active user(s) by percentages of messages: ${reply}`);
+				} else {
+					return interaction.reply(`Top ${top} active user(s) by percentages of messages: ${reply}`);
+				}
+				
 		} catch(error) {
 			console.error(error);
 			console.log(`There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``);
