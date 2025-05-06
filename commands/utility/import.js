f33d93fafe3ece8cf0fdaf2c5a50e21ed236dfe7
Dana Yuan <danayuan2006@gmail.com>
2025-05-06 03:17:02 -0400

    Continued working on import(error currently)

--- commands/utility/import.js
+++ commands/utility/import.js
@@ -11,40 +11,36 @@ module.exports = {
 		const memberCount = `${interaction.guild.memberCount}`;
 		try {
 			let guildIdReal = interaction.guild.id;
-			console.log("=-=-=-=-=-=-=");
-			//console.log(guildIdReal);
 			let channels = client.channels.cache.filter(ch => {
 				console.log(typeof ch);
-				return ch.guild.id === guildIdReal && typeof ch === TextChannel;
+				return ch.guild.id === guildIdReal && ch.lastMessageId != null;
 			})
-			//let channel = client.channels.cache.get('1358621798195003606');
 			let msgs = [];
 			let usrs = [];
 			let dts = [];
-			//console.log(client.channels.cache);
-			console.log("=-=-=-=-=-=-=");
-			console.log(channels);
 
 			await interaction.deferReply();
 
-			/*let ptr = await channel.messages.fetch({ limit: 1 })
-				.then(messages => (messages.size === 1 ? messages.first() : null));*/
+			channels.forEach(channel => {
+				let ptr = await channel.messages.fetch({ limit: 1 })
+				.then(messages => (messages.size === 1 ? messages.first() : null));
 
-			/*while (ptr) {
-				await channel.messages
-					.fetch({ limit: 100, before: ptr.id })
-					.then(messages => {
-						messages.forEach(message => {
-							if (!message.author.bot) {
-								msgs.push(message.content);
-								usrs.push(message.author.username);
-								const dt = new Date(message.createdTimestamp * 1000).toLocaleString("en-US")
-								dts.push(dt);
-							} 
-						})
-						ptr = 0 < messages.size ? messages.at(messages.size - 1) : null;
-					});
-			}*/
+				while (ptr) {
+					await channel.messages
+						.fetch({ limit: 100, before: ptr.id })
+						.then(messages => {
+							messages.forEach(message => {
+								if (!message.author.bot) {
+									msgs.push(message.content);
+									usrs.push(message.author.username);
+									const dt = new Date(message.createdTimestamp * 1000).toLocaleString("en-US")
+									dts.push(dt);
+								} 
+							})
+							ptr = 0 < messages.size ? messages.at(messages.size - 1) : null;
+						});
+				}
+			})
 
 			//console.log(msgs + " \n" + usrs + " \n" + dts);
 			interaction.editReply(`Messages:\n${msgs}`);  // Print all messages
